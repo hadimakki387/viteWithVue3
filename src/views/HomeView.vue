66f4bb3e90494a64edbdccd30490456b8f44399e
hadimakki387 <hmakki387@gmail.com>
2023-04-30 15:36:38 +0300

    extracted into composable item
    extracted the functoins related to the input tracking into its own composable file to make it reusable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,11 @@
 <script setup>
-import {useFlash} from "@/composables/useFLash";
-import { ref, watch } from "vue";
-let {flash} = useFlash();
+import { useFlash } from '@/composables/useFLash'
+import { useStorage } from '@/composables/useStorage'
 
-let food = ref(localStorage.getItem('food'));
-let age = ref(localStorage.getItem('age'));
+let { flash } = useFlash();
 
-function  write(key,val){
-  localStorage.setItem(key,val)
-}
-
-
-watch(food , (val)=>{
-  write('food',val);
-});
+let food = useStorage('food', 'salad');
+let age = useStorage('age');
 
 </script>
 
@@ -21,11 +13,7 @@ watch(food , (val)=>{
   <main>
     <TheWelcome />
     <button @click="flash('It works!')">click Me</button>
-    <p>
-    Whats ur fav food <input type="text" v-model="food" >  
-    </p>
-    <p>
-    Whats ur age <input type="text" v-model="age">  
-    </p>
+    <p>Whats ur fav food <input type="text" v-model="food" /></p>
+    <p>how old are you <input type="text" v-model="age" /></p>
   </main>
 </template>
