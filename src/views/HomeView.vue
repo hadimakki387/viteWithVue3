2bdf49989ecb91c5492d7ef66474b79d97d3cf0d
hadimakki387 <hmakki387@gmail.com>
2023-04-30 14:58:46 +0300

    created an inline storing functions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,31 @@
 <script setup>
 import {useFlash} from "@/composables/useFLash";
-
+import { ref, watch } from "vue";
 let {flash} = useFlash();
+
+let food = ref(localStorage.getItem('food'));
+let age = ref(localStorage.getItem('age'));
+
+function  write(key,val){
+  localStorage.setItem(key,val)
+}
+
+
+watch(food , (val)=>{
+  write('food',val);
+});
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
     <button @click="flash('It works!')">click Me</button>
-    
+    <p>
+    Whats ur fav food <input type="text" v-model="food" >  
+    </p>
+    <p>
+    Whats ur age <input type="text" v-model="age">  
+    </p>
   </main>
 </template>
