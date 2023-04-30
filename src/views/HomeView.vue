71d9fc3f44698e723046df12c18b3cac75831fd0
hadimakki387 <hmakki387@gmail.com>
2023-04-30 12:01:08 +0300

    used composables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,13 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-import { ref } from 'vue';
-let message = ref("Hello World");
+import {useFlash} from "@/composables/useFLash";
 
-setTimeout(()=>{
-  message.value="I have changed";
-},2000);
-
+let {flash} = useFlash();
 </script>
 
 <template>
   <main>
     <TheWelcome />
-    <p>{{ message }}</p>
-    <p>
-      <input type="text" name="" id="" v-model="message">
-    </p>
+    <button @click="flash('It works!')">click Me</button>
+    
   </main>
 </template>
